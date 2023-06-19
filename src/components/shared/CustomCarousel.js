d2c9f1098ac41cd905862838363542226589a784
emilianoa3e <emiliano.ortiz@a3e.com.mx>
2023-06-19 13:26:53 -0600

    added filteredList at Carousel

--- src/components/shared/CustomCarousel.js
+++ src/components/shared/CustomCarousel.js
@@ -1,30 +1,28 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import { getBanners } from '../../utils/getBanners';
+import '../../assets/css/components/Carousel.css';
+
 function CustomCarousel() {
-  const [mediaList, setMediaList] = useState([
-    {
-      _id: "",
-      title: "",
-      description: "",
-      image: "",
-      link: "",
-      status: ""
-    },
-  ])
+  const [mediaList, setMediaList] = useState([]);
 
   useEffect(() => {
     const getData = async () => {
       const data = await getBanners();
-      setMediaList(data.banners)
+      setMediaList(data.banners);
     };
     getData();
-  }, [])
+  }, []);
+
+  if (mediaList.length === 0) {
+    return <div>No hay banners disponibles.</div>;
+  }
+
+  const filteredMediaList = mediaList.filter((media) => media.status === true);
 
   return (
-
-    <Carousel style={{margin:0, padding:0}}>
-      {mediaList.map((media) => (
+    <Carousel style={{ margin: 0, padding: 0 }}>
+      {filteredMediaList.map((media) => (
         <Carousel.Item key={media._id}>
           <img
             className="d-block w-100"
@@ -35,12 +33,12 @@ function CustomCarousel() {
           <Carousel.Caption>
             <h3>{media.title}</h3>
             <p>{media.description}</p>
+            <a href={media.link} target='_blank' className='btn btn-success'>Ver más</a>
           </Carousel.Caption>
         </Carousel.Item>
       ))}
     </Carousel>
-
-  )
+  );
 }
 
-export default CustomCarousel
+export default CustomCarousel;
