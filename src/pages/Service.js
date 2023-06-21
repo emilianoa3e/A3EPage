02e7f8f08de9c50819474f844915a3a4146ed8fe
emilianoa3e <emiliano.ortiz@a3e.com.mx>
2023-06-21 14:10:47 -0600

    changed navbar, added Footer and resolve problems in service's Styles

--- src/pages/Service.js
+++ src/pages/Service.js
@@ -1,38 +1,35 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom';
-import { getService } from '../utils/getServices'
-import {Container}  from 'react-bootstrap';
-
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { getService } from "../utils/getServices";
+import { Container } from "react-bootstrap";
+import "../assets/css/pages/Service.css";
 function Service() {
   const { id } = useParams();
   const [service, setService] = useState({
-    _id: '',
-    title: '',
-    content: ''
-  },)
+    _id: "",
+    title: "",
+    content: "",
+  });
 
   useEffect(() => {
-
     const getData = async () => {
       const data = await getService(id);
-      setService(data.service.content)
-    }
+      setService(data.service.content);
+    };
     getData();
-  }, [id])
+  }, [id]);
 
   return (
     <>
       {service && (
-        <Container className='mt-1 p-1 container-fluid ' >
-          <div dangerouslySetInnerHTML={{ __html: service }}></div>
-        </Container>
-
-      )
-
-      }
+        <Container
+          dangerouslySetInnerHTML={{ __html: service }}
+          className="mt-1 p-5 cont"
+          fluid
+        ></Container>
+      )}
     </>
-
-  )
+  );
 }
 
-export default Service
+export default Service;
