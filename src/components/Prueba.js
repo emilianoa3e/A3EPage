63d685905fe598be15fc60c1a5a0cba115389323
emilianoa3e <emiliano.ortiz@a3e.com.mx>
2023-07-07 12:05:44 -0600

    rotate 3D model

--- src/components/Prueba.js
+++ src/components/Prueba.js
@@ -1,11 +1,42 @@
-import React, { useEffect, useRef, Suspense } from "react";
+import React, {  useRef, Suspense, useState } from "react";
 import { Canvas } from "react-three-fiber";
 import { House } from "./House";
 import { OrbitControls, Sky } from "@react-three/drei";
+import { useFrame } from "react-three-fiber";
+import { Col } from "react-bootstrap";
+function RotatingHouse({children}) {
+  const groupRef = useRef();
+  const [isHovered, setIsHovered] = useState(false);
+
+  useFrame(() => {
+    if (!isHovered) {
+      groupRef.current.rotation.y += 0.01; // Ajusta la velocidad de rotación según tus necesidades
+    }
+  });
+
+  const handlePointerEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handlePointerLeave = () => {
+    setIsHovered(false);
+  };
+
+  return (
+    <group
+      ref={groupRef}
+      onPointerEnter={handlePointerEnter}
+      onPointerLeave={handlePointerLeave}
+    >
+      {children}
+    </group>
+  );
+}
+
 function Prueba() {
   return (
     <>
-      <div style={{ height: "100%", width: "100%" }}>
+      <Col style={{height:'100vh'}} className="m-5">
         <Canvas camera={{ zoom: 1, position: [15, 20, 15] }}>
           <ambientLight intensity={0.5} />
           <pointLight position={[35, 35, 0]} intensity={0.4} />
@@ -16,12 +47,14 @@ function Prueba() {
             inclination={0}
             azimuth={0.25}
           />
+          <OrbitControls enablePan={false} maxDistance={40} minDistance={0}/>
           <Suspense fallback={null}>
-            <House />
-            <OrbitControls />
+            <RotatingHouse >
+              <House/>
+            </RotatingHouse>
           </Suspense>
         </Canvas>
-      </div>
+      </Col>
     </>
   );
 }
